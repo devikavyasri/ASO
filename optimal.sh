9131557ee4ae28111f7069693636740d8456538c
devikavyasri <44308278+devikavyasri@users.noreply.github.com>
2020-05-04 10:07:21 -0400

    Add files via upload

--- optimal.sh
+++ optimal.sh
@@ -65,6 +65,24 @@ fi
 #Storing computation start time
 MAIN_START=$(gdate +%s.%N)
 
+
+#checking if input data files exists or not
+if [ ! -f "$generatorInput" ]
+then
+    echo "GeneratorFileDoesNotExist" > temp/solution.txt
+    MAIN_END=$(gdate +%s.%N) #Storing computation end time
+    timeTaken=$(echo "$MAIN_END - $MAIN_START"|bc) #Finding actual computation time
+    echo "timeTaken= "$timeTaken
+    exit
+elif [ ! -f "$preferenceInput" ]
+then
+    echo "PreferenceFileDoesNotExist" > temp/solution.txt
+    MAIN_END=$(gdate +%s.%N) #Storing computation end time
+    timeTaken=$(echo "$MAIN_END - $MAIN_START"|bc) #Finding actual computation time
+    echo "timeTaken= "$timeTaken
+    exit
+fi
+
 #Generate arbitary outcome
 clingo $generatorInput generatorProgram.lp $preferenceInput $calculateSatisfatctionDegree > temp/tempAnswer.txt
 
